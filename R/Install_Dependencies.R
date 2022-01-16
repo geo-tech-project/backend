5f49687757cf799d52b2ed24a7b6eea74aab92c6
fbruc03 <65135023+fbruc03@users.noreply.github.com>
2022-01-16 16:32:57 +0100

    init

--- R/Install_Dependencies.R
+++ R/Install_Dependencies.R
@@ -1,20 +1,27 @@
 
-GetSatelliteImages_dependencies <- c("terra","sf","rgdal","rgeos","rstac","gdalcubes","raster")
+# GetSatelliteImages_dependencies <- c("terra","sf","rgdal","rgeos","rstac","gdalcubes","raster")
 
-#Itereate over the list of dependencies and install them
-for (dependencies in GetSatelliteImages_dependencies) {
-    #if dependency is not installed.packages() install it
-    if (!(dependencies %in% installed.packages())) {
-        install.packages(dependencies, repos="http://cran.us.r-project.org")
-    }
+# #Itereate over the list of dependencies and install them
+# for (dependencies in GetSatelliteImages_dependencies) {
+#     #if dependency is not installed.packages() install it
+#     if (!(dependencies %in% installed.packages())) {
+#         install.packages(dependencies, repos="http://cran.us.r-project.org")
+#     }
     
-}
+# }
 
-ML_AOA_dependencies <- c("raster","caret","CAST","lattice","sf","Orcs","jsonlite","tmap","latticeExtra","doParallel","parallel","Orcs","sp","rgeos","geojson","rjson","randomForest")
+# ML_AOA_dependencies <- c("raster","caret","CAST","lattice","sf","Orcs","jsonlite","tmap","latticeExtra","doParallel","parallel","Orcs","sp","rgeos","geojson","rjson","randomForest")
 
-for(dependencies in ML_AOA_dependencies) {
-    if (!(dependencies %in% installed.packages())) {
-        install.packages(dependencies, repos="http://cran.us.r-project.org")
-    }
-}
+# for(dependencies in ML_AOA_dependencies) {
+#     if (!(dependencies %in% installed.packages())) {
+#         install.packages(dependencies, repos="http://cran.us.r-project.org")
+#     }
+# }
 
+#install.packages('terra')
+#install.packages('rgdal')
+# install.packages('sf')
+# install.packages('rgeos')
+#install.packages('rstac')
+#install.packages('gdalcubes')
+#install.packages('raster')
