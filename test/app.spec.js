c130c3283c3ebfbf7d9fa140625a94f6e1365815
Hes097 <henning097@live.com>
2022-01-19 17:18:20 +0100

    changing demo tests

--- test/app.spec.js
+++ test/app.spec.js
@@ -4,12 +4,32 @@ const app = require("../app.js")
 
 
 //Test of demo route
-describe("GET /rundemo", function () {
-    it("Should return status code 201", async function () {
+describe("POST /start", function () {
+    it("Should return status code 200", async function () {
         await request(app)
-            .get("/rundemo")
+            .post("/start")
+            .send({
+                whereareyoufrom: 'demo',
+                topleftlat: 51.946286720328104,
+                topleftlng: 7.5971644627228905,
+                bottomleftlat: 51.975309509611826,
+                bottomleftlng: 7.5971644627228905,
+                bottomrightlat: 51.975309509611826,
+                bottomrightlng: 7.652018947455736,
+                toprightlat: 51.946286720328104,
+                toprightlng: 7.652018947455736,
+                option: 'model',
+                algorithm: 'rf',
+                startDate: '2021-05-31T22:00:00.000Z',
+                endDate: '2021-08-30T22:00:00.000Z',
+                filename: 'model.RDS',
+                resolution: '10',
+                channels: ['B02', 'B03', 'B04', 'SCL'],
+                coverage: 20,
+                mtry: 2
+            })
             .expect(200)
-    })
+    }).timeout(40000)
 })
 
 
@@ -40,5 +60,5 @@ describe("POST /start", function () {
                 mtry: null
             })
             .expect(200)
-    })
+    }).timeout(300000)
 })
